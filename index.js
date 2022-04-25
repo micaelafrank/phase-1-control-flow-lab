5c84e7510c46db8b1ac9ea83cc9322e7899c133a
Micaela Frank <micaelalfrank@gmail.com>
2022-04-25 16:18:17 -0400

    commit tree cf874a5c9a5e3ea43a832eb220a4d5aaec7005d4

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length){
+  if (length <= 400){
+    console.log("This one is on me!");
+    return "This one is on me!";
+  }
+  else if (length > 400 && length < 2000){
+    console.log("That will be twenty bucks.");
+    return "That will be twenty bucks.";
+  }
+  else if (length > 2000 && length < 2500){
+    console.log("I will gladly take your thirty bucks.");
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (length > 2500){
+    console.log("No can do.");
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(string){
+  if (string === "NYC"){
+    return "Ok, sounds good.";
+  }
+  else{
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  /*switch (tip){
+    case 'generous':
+      console.log("Thank you so much.");
+      break;
+    case 'not as generous':
+      console.log("Thank you.");
+      break;
+    default:
+      console.log("Bye.");
+  } */
+
+  if (tip === 'generous'){
+    return "Thank you so much.";
+  }
+  else if (tip === "not as generous"){
+    return "Thank you.";
+  }
+  else{
+    return "Bye.";
+  }
 }
